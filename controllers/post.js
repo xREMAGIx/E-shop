080ab813a8db4dd8f58366b375b88b62bcdf7c59
fayewong6198 <trandaosimanh@gmail.com>
2020-04-12 16:24:12 +0700

    fix bug posts

--- controllers/post.js
+++ controllers/post.js
@@ -17,7 +17,7 @@ exports.getPosts = asyncHandler(async (req, res, next) => {
 // @route POST /api/posts/:id
 // @access  Private
 exports.getPost = asyncHandler(async (req, res, next) => {
-  const post = Post.findById(req.params.id).populate("Image");
+  const post = await Post.findById(req.params.id).populate("Image");
 
   if (!post) return next(new ErrorResponse("Resource not found", 404));
 
@@ -28,7 +28,7 @@ exports.getPost = asyncHandler(async (req, res, next) => {
 // @route GET /api/posts/:userId
 // @access  Private
 exports.getUserPost = asyncHandler(async (req, res, next) => {
-  const posts = Post.find({ user: req.params.userId }).populate("Image");
+  const posts = await Post.find({ user: req.params.userId }).populate("Image");
 
   return res.status(200).json({ success: true, data: posts });
 });
@@ -38,7 +38,7 @@ exports.getUserPost = asyncHandler(async (req, res, next) => {
 // @access  Private
 exports.createPost = asyncHandler(async (req, res, next) => {
   const { title, content } = req.body;
-  const post = Post.create({ user: req.user.id, title, content });
+  const post = await Post.create({ user: req.user.id, title, content });
 
   return res.status(200).json({ success: true, data: post });
 });
@@ -47,7 +47,7 @@ exports.createPost = asyncHandler(async (req, res, next) => {
 // @route PUT /api/posts/:id
 // @access  Private
 exports.updatePost = asyncHandler(async (req, res, next) => {
-  const post = Post.findById(req.params.id);
+  const post = await Post.findById(req.params.id);
 
   if (post.user != req.user.id && req.user.role != "admin") {
     return new ErrorResponse(`You can not access this route`, 403);
@@ -62,13 +62,14 @@ exports.updatePost = asyncHandler(async (req, res, next) => {
 // @route Delete /api/posts/:id
 // @access  Private
 exports.deletePost = asyncHandler(async (req, res, next) => {
-  const post = Post.findById(req.params.id);
+  const post = await Post.findById(req.params.id);
 
   if (post.user != req.user.id && req.user.role != "admin") {
     return new ErrorResponse(`You can not access this route`, 403);
   }
+  console.log("con cac");
 
-  await post.remove();
+  await Post.findByIdAndRemove(req.params.id);
 
   return res.status(200).json({ success: true, data: [] });
 });
@@ -87,7 +88,7 @@ exports.postImageUpload = asyncHandler(async (req, res, next) => {
   // Create custom filename
   const image = await Image.create({
     user: req.user.id,
-    post: req.params.id
+    post: req.params.id,
   });
 
   const file = req.files.file;
@@ -96,7 +97,7 @@ exports.postImageUpload = asyncHandler(async (req, res, next) => {
   image.path = file.name;
   await image.save();
   console.log(file.name);
-  file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async err => {
+  file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async (err) => {
     if (err) {
       console.error(err);
       // Delete Image
